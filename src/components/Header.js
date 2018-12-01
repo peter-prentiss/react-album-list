26e351f3b449cf45e03b8e97e3703b857761ca78
Peter <peter.prentiss@gmail.com>
2018-12-01 15:22:05 -0600

    Update header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,14 @@ const Header = () => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2
     },
     textStyle: {
         fontSize: 20
