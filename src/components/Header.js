037cb58ec5c42ac900062ecb6fe58f198314a639
Peter <peter.prentiss@gmail.com>
2018-12-01 15:12:33 -0600

    Delete app and use view in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
+    const { textStyle, viewStyle } = styles;
+
     return (
-        <Text>Albums</Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
     );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 export default Header;
